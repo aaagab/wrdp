60b94efa445a906f0c6ece70a3e40de96657409e
gabaaa <gabaaa.dev@gmail.com>
2024-09-05 15:51:14 -0500

    First commit after clone.

---
+++ __init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# authors: Gabriel Auger
+# name: xfreerdp wrapper
+# licenses: MIT 
+__version__ = "0.1.0"
+
+# from .dev.wrdp import wrdp
+# from .gpkgs import message as msg
+# from .gpkgs.nargs import Nargs
+# from .gpkgs.etconf import Etconf
